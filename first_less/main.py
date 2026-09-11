8c44b3a03218f0530768441452de4726df53dcf0
plozzz126 <216751339+plozzz126@users.noreply.github.com>
2026-09-12 02:56:02 +0500

    updated algorithm, added validation and SRP

--- first_less/main.py
+++ first_less/main.py
@@ -8,49 +8,94 @@
 
 # В ответ компьютер показывает число отгаданных цифр, стоящих на своих местах (число быков) и число отгаданных цифр, стоящих не на своих местах (число коров).
 
+#добавил принцип единой ответственности ну от части. добавил валидацию на вводе 
+
 import random
-a = ""
 
-herd = int(input("введите кол-во чисел в загадке: "))
-sumH = 1
+def input_user():
+    while True:
+        try:
+            herd = int(input("введите кол-во чисел в загадке: "))
+            if herd > 10:
+                print("более 10 чисел нельзя")
+                continue
+            elif herd <= 0:
+                print("нельзя вводить отрицательные числа и 0")
+                continue
+            return herd
+        except ValueError:
+            print("Ошибка, введите целое число")
+        
+    
+herd = input_user()
 
-for i in range(herd):
-    sumH *= 10
-    a += str(random.randint(0, 9))
+def gen_num():
+    a = ""
+    used_num = []
+    
+    while len(a) < herd:
+        num = str(random.randint(0, 9))
+       
+        if num not in used_num:
+            used_num.append(num)
+            a += num
+    return a
 
+a = gen_num()
 print(a)
 
-while True:
-    num = input(f"введите {herd} числа: ")
-    if len(num) < herd:
-        print(f"меньше {herd}: ")
-        continue
-    elif len(num) > herd:
-        print(f"больше {herd}: ")
-        continue
-    
-    bulls = 0
-    cow = 0
+def validation_num():
+    while True:
+        num = input(f"введите {herd} чисел/ла: ")
 
-    for i in range(herd): 
-        if num[i] == a[i]:
-            bulls += 1
-        else:
+        if not num.isdigit():
+            print("Ошибка, введите числа")
             continue
+        
+        if len(num) < herd:
+            print(f"меньше {herd}: ")
+            continue
+        elif len(num) > herd:
+            print(f"больше {herd}: ")
+            continue
+        return num    
 
-    for i in range(herd):
-        for j in range(herd):
-            if i != j and num[i] == a[j]:
-                cow += 1
-            else:
-                continue
-
+def output(bulls, cow):
     print("число быков: ", bulls)
     print("число коров: ", cow)
 
-    if bulls == herd:
-        print("победа")
-        break
+def counter():    
+    attempts = 0
+    while True:
+        bulls = 0
+        cow = 0
+        useless_num = []
+        num_bull = []
+        num = validation_num()
+
+        for i in range(herd): 
+            if num[i] == a[i]:
+                bulls += 1
+                num_bull.append(i)
+
+        for i in range(herd):
+            for j in range(herd):
+                if i != j and j not in useless_num and i not in num_bull and num[i] == a[j]:
+                    cow += 1
+                    useless_num.append(j)
+                    break
+
+        attempts += 1
+        output(bulls, cow)
+
+        if bulls == herd:
+            print(f"победа \n вы угадали число за {attempts} попыток")
+            break
+
+counter()
+
+
+
 
 
 # версия 2.0 
@@ -58,7 +103,6 @@ while True:
 # 2. проверка ввода 
 
 # не использовал но в теории мог
-
 # for i in range(herd):
 # a = random.randint(0000, sumH - 1)
 # print(str(a).zfill(herd))
